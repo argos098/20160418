06a1e83edba8fc1f990eb2b7580544403812e11b
argos098 <argos098@gmail.com>
2016-04-19 10:50:03 +0900

    안드로이드 수업

--- T04_Activity/app/src/main/java/com/example/a/t04_activity/MainActivity.java
+++ T04_Activity/app/src/main/java/com/example/a/t04_activity/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.a.t04_activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] list = {"hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, list[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
