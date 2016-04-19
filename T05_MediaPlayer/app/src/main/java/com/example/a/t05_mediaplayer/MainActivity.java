e026c65b68b28133215593a0e03570a07337c4bd
argos098 <argos098@gmail.com>
2016-04-19 14:47:34 +0900

    안드로이드 수업

--- T05_MediaPlayer/app/src/main/java/com/example/a/t05_mediaplayer/MainActivity.java
+++ T05_MediaPlayer/app/src/main/java/com/example/a/t05_mediaplayer/MainActivity.java
@@ -3,19 +3,56 @@ package com.example.a.t05_mediaplayer;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.SeekBar;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int SEEKBAR_VALUE = 1;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == SEEKBAR_VALUE) {
+                seekBar.setProgress(msg.arg1);
+            }
+        }
+    };
+
     MediaPlayer mp = null;
+    SeekBar seekBar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar) findViewById(R.id.seekbar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser==true){
+                    if(mp != null){
+                        mp.seekTo(progress);
+                    }
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void onStartClick(View v){
@@ -28,6 +65,27 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         mp.start();
+        seekBar.setMax(mp.getDuration());
+
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(mp != null){
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if(mp != null) {
+                        Message msg = new Message();
+                        msg.what = SEEKBAR_VALUE;
+                        msg.arg1 = mp.getCurrentPosition();
+                        handler.sendMessage(msg);
+                    }
+                }
+            }
+        });
+        th.start();
     }
 
     public void onStopClick(View v){
