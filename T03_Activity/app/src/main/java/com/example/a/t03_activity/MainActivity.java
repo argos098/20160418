eba47c4582c13aae8e5c9a36c1e5e8b1b464df30
argos098 <argos098@gmail.com>
2016-04-19 10:13:37 +0900

    안드로이드 수업

--- T03_Activity/app/src/main/java/com/example/a/t03_activity/MainActivity.java
+++ T03_Activity/app/src/main/java/com/example/a/t03_activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.a.t03_activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("id");
+        String pw = intent.getStringExtra("pw");
+        Toast.makeText(this, id, Toast.LENGTH_SHORT).show();
     }
+
+    public void onFinishButtonClick(View view){
+        Intent intent = new Intent();
+        intent.putExtra("myResult", "abcdef");
+        setResult(RESULT_OK, intent);
+
+        finish();
+    }
+
 }
---
+++ T03_Activity/app/src/main/java/com/example/a/t03_activity/MyActivity.java
@@ -0,0 +1,33 @@
+package com.example.a.t03_activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MyActivity extends AppCompatActivity {
+    final int MY_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+    public void onStartButtonClick(Intent view){
+        Intent intent = new Intent(MyActivity.this, MainActivity.class);
+        intent.putExtra("id","abcd");
+        intent.putExtra("pw","efgh");
+        startActivityForResult(intent, MY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == MY_REQUEST){
+            if(resultCode == RESULT_OK){
+                String res = data.getStringExtra("myResult");
+                Toast.makeText(this, res, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
