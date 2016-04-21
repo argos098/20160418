0c0a20c82d20a3c06e7794655582495eb9276278
argos098 <argos098@gmail.com>
2016-04-21 10:57:12 +0900

    안드로이드 수업

--- T15_Service/app/src/main/java/com/example/a/t15_service/MyService.java
+++ T15_Service/app/src/main/java/com/example/a/t15_service/MyService.java
@@ -3,6 +3,7 @@ package com.example.a.t15_service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 /**
  * Created by a on 2016-04-21.
@@ -14,6 +15,26 @@ public class MyService extends Service{
 
     @Override
     public IBinder onBind(Intent intent) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException("Not yet implement");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.d("service", "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("service", "onStartCommand");
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d("service", "onDestroy");
+        super.onDestroy();
     }
 }
