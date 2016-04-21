ae0236e8eb8fa1ccfc396fd60b244ffc6a59d331
argos098 <argos098@gmail.com>
2016-04-21 14:01:23 +0900

    안드로이드 수업

--- T17_Location/app/src/main/java/com/example/a/t17_location/MainActivity.java
+++ T17_Location/app/src/main/java/com/example/a/t17_location/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.a.t17_location;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -67,5 +74,43 @@ public class MainActivity extends AppCompatActivity {
         }
         manager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
                 0, 0, listener);
+        /*
+        manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                0, 0, listener);
+                */
+         geocoder = new Geocoder(this);
+    }
+
+    Geocoder geocoder;
+    public void onReverseGeocodingClick(View v){
+        EditText editLat = (EditText) findViewById(R.id.editLat);
+        EditText editLon = (EditText) findViewById(R.id.editLon);
+
+        double lat = Double.parseDouble(editLat.getText().toString());
+        double lon = Double.parseDouble(editLon.getText().toString());
+
+        try {
+            List<Address> list = geocoder.getFromLocation(lat, lon, 10);
+            if(list.size()>0){
+                Address address = list.get(0);
+                //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+                Log.d("geododing", address.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onGeocodingClick(View v){
+        EditText editName = (EditText) findViewById(R.id.editLat);
+        String str = editName.getText().toString();
+        try {
+            List<Address> list = geocoder.getFromLocationName(str, 10);
+            Address address = list.get(0);
+            Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+            Log.d("geocoding", address.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
